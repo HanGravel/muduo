18b600989371608378066324aadebbec4d3845fc
Wang Tuo <wangtuo.coder@gmail.com>
2017-05-10 13:40:46 +0800

    Update CurrentThread.h

--- muduo/base/CurrentThread.h
+++ muduo/base/CurrentThread.h
@@ -12,6 +12,9 @@ namespace muduo
 {
 namespace CurrentThread
 {
+  /*
+  * __thread 代表每个线程各自保有而非共享的变量。 gcc内置的关键字
+  */
   // internal
   extern __thread int t_cachedTid;
   extern __thread char t_tidString[32];
@@ -19,6 +22,13 @@ namespace CurrentThread
   extern __thread const char* t_threadName;
   void cacheTid();
 
+  /*
+  * inline关键字相当于于预处理器中宏定义函数，使运行时更快。多用于简单函数。
+  * __builtin_expect(expr,int) 是gcc内置函数，作用是分支优化预测优化。
+  * expr大部分情况是false时，__builtin_expect(expr,0)在运行时效率更高
+  * expr大部分情况是true时，__builtin_expect(expr,0)在运行时效率更高
+  */
+  
   inline int tid()
   {
     if (__builtin_expect(t_cachedTid == 0, 0))
