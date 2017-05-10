5fdfa5941ad6a6a021774e513b9f163c20708037
Wang Tuo <wangtuo.coder@gmail.com>
2017-05-10 14:39:25 +0800

    Update Mutex.h

--- muduo/base/Mutex.h
+++ muduo/base/Mutex.h
@@ -13,6 +13,16 @@
 
 #ifdef CHECK_PTHREAD_RETURN_VALUE
 
+/*
+* 在cdefs.h头文件中有定义
+* #define __BEGIN_DECLS extern "C" {
+* #define __END_DECLS }
+* 这代表编译器将以C语言的方式编译这段代码
+*
+* 在assert.h头文件中，
+* 如果def了NDEBUG，将不会声明__assert_perror_fail函数，此处作者手动添加了此函数声明,以供MCHECK宏使用
+*/
+
 #ifdef NDEBUG
 __BEGIN_DECLS
 extern void __assert_perror_fail (int errnum,
@@ -22,6 +32,11 @@ extern void __assert_perror_fail (int errnum,
     __THROW __attribute__ ((__noreturn__));
 __END_DECLS
 #endif
+/*
+* MCHECK是检查返回值的宏。意义在于防止ENOMEM之类的资源不足情况。
+* MCHECK所做的就是一旦检查出这种错误，就立刻清理现场并主动退出。
+* 检查的是返回值是否等于0
+*/
 
 #define MCHECK(ret) ({ __typeof__ (ret) errnum = (ret);         \
                        if (__builtin_expect(errnum != 0, 0))    \
@@ -54,6 +69,9 @@ class MutexLock : boost::noncopyable
   MutexLock()
     : holder_(0)
   {
+      /*
+      * pthread_mutex_init和prthread_mutex_destroy成功时返回0，否则返回大于0的错误代码
+      */
     MCHECK(pthread_mutex_init(&mutex_, NULL));
   }
 
@@ -94,8 +112,17 @@ class MutexLock : boost::noncopyable
   }
 
  private:
+  
+ /*
+ * 声明Condition为友元
+ */
   friend class Condition;
-
+  
+  /*
+  * 定义一个嵌套类。
+  * 嵌套类的成员不属于外围类，外围类的成员也不属于嵌套类，两者之间的访问没有任何特权
+  */
+  
   class UnassignGuard : boost::noncopyable
   {
    public:
